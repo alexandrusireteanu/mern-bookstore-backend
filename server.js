7620e5a62db50ec0025aa017778173a69b8de890
Alexandru Sireteanu <alexandru.sireteanu@Alexandrus-Mac-mini.local>
2025-12-01 12:48:32 +0200

    second commit

--- server.js
+++ server.js
@@ -20,9 +20,8 @@ const PORT = 3000;
 // Configurarea middleware-ului de bază
 // Configurează CORS pentru producție
 app.use(cors({
-  origin: process.env.CLIENT_URL || 'http://localhost:5173',
-  credentials: true,
-  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS']
+  origin: 'https://mern-bookstore-frontend-pi.vercel.app',
+  credentials: true
 }));
 app.use(express.json()); // Parser pentru JSON în request body
 
